b7dca277c1b9ba931cd9804066b8e5e81d5b1b60
duoleyisui <1694474987@qq.com>
2018-08-11 11:49:21 +0800

    A
    A

--- 购物APP/js/guide.js
+++
@@ -1,9 +0,0 @@
-var mySwiper = new Swiper('.swiper-container',{
-		pagination : '.swiper-pagination',
-		direction:'horizontal',
-		});
-		//$(function(){
-			$('.enjoy').click(function(){
-				localStorage.setItem("install",true);
-				window.location.href='newindex.html';
-		});
